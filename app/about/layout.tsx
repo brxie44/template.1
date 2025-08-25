873337116504ace51951e579f886693a62ef6b25
Sean Quillen <squillen@paypal.com>
2025-08-25 09:48:26 -0500

    add header to each page

---
+++ app/about/layout.tsx
@@ -0,0 +1,18 @@
+import type React from "react"
+import { Header } from "@/uicomponents/store/header"
+import { CartProvider } from "@/uicomponents/store/cart-provider"
+
+export default function AboutLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <CartProvider>
+      <div className="min-h-screen flex flex-col">
+        <Header />
+        <main className="flex-1">{children}</main>
+      </div>
+    </CartProvider>
+  )
+}
